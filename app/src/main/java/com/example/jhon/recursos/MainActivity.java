7e859d59fc99128d4abffcc5b5be88595cc6520c
Jhon <jftrujillo@unicauca.edu.co>
2016-09-28 07:21:40 -0500

    First commit

---
+++ app/src/main/java/com/example/jhon/recursos/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.jhon.recursos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ListView list;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        list = (ListView) findViewById(R.id.list);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.mascotas);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/jhon/recursos/models/Mascota.java
@@ -0,0 +1,74 @@
+package com.example.jhon.recursos.models;
+
+/**
+ * Created by jhon on 23/09/16.
+ */
+public class Mascota {
+    public static final int ENFERMO = 0;
+    public static final int SANO = 1;
+    public static final int URGENTE = 2;
+
+    String raza;
+    String color;
+    String descripcion;
+    String urlImg;
+    String nombre;
+    int estado;
+    String dineroNecesitado;
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getDineroNecesitado() {
+        return dineroNecesitado;
+    }
+
+    public void setDineroNecesitado(String dineroNecesitado) {
+        this.dineroNecesitado = dineroNecesitado;
+    }
+}
